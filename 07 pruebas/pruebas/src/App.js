6a803dbf97b559488f93c9e4318a2b355069fab3
Jose Aguilera <joseaguilera1709@gmail.com>
2023-02-02 11:46:38 -0300

    Proyecto de Prueba

---
+++ 07 pruebas/pruebas/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import LightSwitch from './components/LightSwitch';
+
+function App() {
+  return (
+      <LightSwitch />
+    );
+    }
+
+
+export default App;
